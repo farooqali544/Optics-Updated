f05e8c46b181f29aca191d43f1bba8fc1e425db5
Usmanalpha37 <muhammadusman121236321@gmail.com>
2022-02-14 19:59:39 -0800

    https://github.com/farooqali544555/Optics-Updated.git

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -6,6 +6,11 @@ const Home = () => {
     const navigate = useNavigate();
 
   return <div className="home">
+
+
+
+<section className='brandingName'>
+<h1 className='title'>Eye Care Optics</h1>
 {/* 
       <div className="addButton">
       <button onClick={()=>{navigate("/add")}}>Add Data</button>
@@ -18,6 +23,16 @@ const Home = () => {
  */}
 
 
+
+ {/* Buttons */}
+
+ <button class="glow-on-hover" type="button" onClick={()=>{navigate("/add")}}>Add Data</button>
+
+
+ <button class="glow-on-hover" type="button" onClick={() =>{navigate("/preview")}}>Preview Data</button>
+
+
+</section>
   </div>;
 };
 
